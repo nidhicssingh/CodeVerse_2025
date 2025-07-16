59bbedbef64123a7a51c83fa10f6b58a312d58a6
Ramanee Kaanth R S <rsramaneekaanth@gmail.com>
2025-07-16 11:35:27 -0700

    Add files via upload

---
+++ CV04_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/Pywhatkit_Whatsapp_message_generator/1_RamaneeKaanth_Normal_WhatsApp.py
@@ -0,0 +1,6 @@
+import pywhatkit
+ 
+# Same as above but Closes the Tab in 60 Seconds after Sending the Message
+pywhatkit.sendwhatmsg("+918754973733", "Hi", 9, 23
+                      , 15, True, 15)
+#
---
+++ CV04_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/Pywhatkit_Whatsapp_message_generator/1_RamaneeKaanth_WhatsApp_with_Number.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+# Send a WhatsApp Message to a Contact at 10:50 AM
+
+pywhatkit.sendwhatmsg("+919281038881", 
+					"Hey there, I'm RamaneeKaanth, this is a test message sent using my python script!", 
+					15, 47)
+ 
