8a75b840eeccca6d32534459d21f74313ba6a2a3
nidhicssingh <nidhi_singh@svyasa.edu.in>
2025-07-16 16:40:22 +0530

    add

--- CV26_CyberKnights/2_ADVANCED PYTHON PROBLEM/6.py
+++
@@ -1,6 +0,0 @@
-import pywhatkit
- 
-# Same as above but Closes the Tab in 60 Seconds after Sending the Message
-pywhatkit.sendwhatmsg("+919741785346", "Hi", 9, 23
-                      , 15, True, 15)
-
