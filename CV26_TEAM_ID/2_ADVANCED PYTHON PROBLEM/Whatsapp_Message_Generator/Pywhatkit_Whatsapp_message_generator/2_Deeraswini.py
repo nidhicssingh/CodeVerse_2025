2872466b8b2efc3866df171a526031be89a72137
deeraswini <deerareddy21@gmail.com>
2025-07-17 20:33:16 +0530

    Add files via upload

---
+++ CV26_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/Pywhatkit_Whatsapp_message_generator/2_Deeraswini.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+# Send a WhatsApp Message to a Contact at 11:26 AM
+
+pywhatkit.sendwhatmsg("+919281038881", 
+					"Hey there, I'm Deeraswini, this is a test message sent using my python script!", 
+					15, 47)
+ 
