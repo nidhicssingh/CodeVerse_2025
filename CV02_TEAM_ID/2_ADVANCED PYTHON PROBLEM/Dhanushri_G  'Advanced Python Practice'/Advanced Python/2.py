62781507ad6ba79cdba692b9125bdb749baee5fe
dhanushri-g <dhanushrigp@gmail.com>
2025-07-17 17:51:44 +0530

    Add files via upload

---
+++ CV02_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Dhanushri_G  'Advanced Python Practice'/Advanced Python/2.py
@@ -0,0 +1,32 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+#Spotify developer account: https://developer.spotify.com
+#goto dashboard create app
+#Redirect URIs*(https://localhost:8888/callback)
+# Replace these with your own credentials
+CLIENT_ID = "fa11c40f5728423298c8d70aaa8ea3b1"
+CLIENT_SECRET = "f1bd078c08094978853baf5948b8b54a"
+
+# Auth Manager
+auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+def search_song(query):
+    results = sp.search(q=query, limit=5, type='track')
+    tracks = results['tracks']['items']
+
+    if not tracks:
+        print("❌ No results found.")
+        return
+
+    print("\n🎧 Top Results:")
+    for i, track in enumerate(tracks):
+        name = track['name']
+        artist = track['artists'][0]['name']
+        album = track['album']['name']
+        url = track['external_urls']['spotify']
+        print(f"{i+1}. {name} by {artist} | Album: {album}\n   🔗 {url}")
+
+# Example usage
+query = input("🎵 Enter a song or artist name to search on Spotify: ")
+search_song(query)
---
+++ CV02_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Dhanushri_G  'Advanced Python Practice'/Pywhatkit/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+# Convert an image to ASCII art
+pywhatkit.image_to_ascii_art('E:/CodeVerse/Hands on python/2_1_2_pywhatkit_Automation/FLOWER.jpg', 'E:/CodeVerse/Hands on python/2_1_2_pywhatkit_Automation/ascii_output')
