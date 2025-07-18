2a0aa9ad903ecf45a4de7f8a7239d1ccfbba2041
deeraswini <deerareddy21@gmail.com>
2025-07-18 23:13:26 +0530

    Add files via upload

---
+++ CV26_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/4_Deeraswini.py
@@ -0,0 +1,6 @@
+from pywhatkit import image_to_ascii_art
+
+image_to_ascii_art(
+    r'C:\Users\deeraswini\Documents\Pywhatit\Pywhatit_Automation\Flower.jpg',
+    r'C:\Users\deeraswini\Documents\Pywhatit\Pywhatit_Automation\ascii_output'
+)
