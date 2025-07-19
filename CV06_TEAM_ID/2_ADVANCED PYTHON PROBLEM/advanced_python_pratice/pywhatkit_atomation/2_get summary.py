ceef1feff21fd2d3b4ac464dba336317aca63d86
NITHYA123-hub <nithyagowda367@gmail.com>
2025-07-19 20:55:03 +0530

    Delete CV06_TEAM_ID/2_ADVANCED PYTHON PROBLEM/advanced_python_pratice directory

--- CV06_TEAM_ID/2_ADVANCED PYTHON PROBLEM/advanced_python_pratice/pywhatkit_atomation/2_get summary.py
+++
@@ -1,3 +0,0 @@
-import pywhatkit
-info = pywhatkit.info("deep learning", lines=5, return_value=True)
-print("Summary:\n", info)
--- CV06_TEAM_ID/2_ADVANCED PYTHON PROBLEM/advanced_python_pratice/pywhatkit_atomation/nithya_1_get info using wiki.py
+++
@@ -1,4 +0,0 @@
-import pywhatkit
-
-# Get a summary about Python
-pywhatkit.info("Python programming language", lines=10)
