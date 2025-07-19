de9f09cd36b1ab25bbefb1c59d9248cae1f656dc
manasa587 <manasasv106@gmail.com>
2025-07-19 18:41:42 +0530

    Create 2__Manasa.py

---
+++ CV18_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/Pywhatkit_Whatsapp_message_generator/2__Manasa.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+# Send a WhatsApp Message to a Contact at 11:26 AM
+
+pywhatkit.sendwhatmsg("+917353438314", 
+					"Hey there, I'm Deeraswini, this is a test message sent using my python script!", 
+					18, 36)
