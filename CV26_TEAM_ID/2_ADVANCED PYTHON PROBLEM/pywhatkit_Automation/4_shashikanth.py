08ddb2282f99673901eddc252357d794de0a6c94
Shashikanth004 <kanthshashi755@gmail.com>
2025-07-19 16:31:02 +0530

    Add files via upload

---
+++ CV26_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/4_shashikanth.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+# Convert an image to ASCII art
+pywhatkit.image_to_ascii_art("C:\Users\kanth\Music\pywhatkit_Automation\fl.jpg", 'E:/CodeVerse/Hands on python/2_1_2_pywhatkit_Automation/ascii_output')
---
+++ CV26_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/6_shashikanth.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+# Convert string to handwriting and save it as an image
+pywhatkit.text_to_handwriting("Hello Nidhi, this is Python magic!", save_to="3.png")
+
+print("Handwriting image saved as '3.png'")
