ad1cb6995e70c0d2f9c0ef4f5cd2459f38fbee69
Tarun-A-19 <tarun192006@gmail.com>
2025-07-19 20:50:46 +0530

    TaskB Advanced Python Practice

---
+++ CV20_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/Tarun A/4_Tarun_A_CV20.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+# Convert an image to ASCII art
+pywhatkit.image_to_ascii_art('C:/College/Notes/SSP/Python/Task(Advanced Python Practice)/pywhatkit_Automation/FLOWER.jpg', 'C:/College/Notes/SSP/Python/Task(Advanced Python Practice)/pywhatkit_Automation/ascii_output')
---
+++ CV20_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/Tarun A/7_Tarun_A_CV20.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting("Hello, Tarun!", save_to="C:/College/Notes/SSP/Python/Task(Advanced Python Practice)/pywhatkit_Automation/output.png")
