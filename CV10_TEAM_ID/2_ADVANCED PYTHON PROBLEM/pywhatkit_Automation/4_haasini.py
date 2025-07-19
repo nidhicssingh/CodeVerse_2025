c231861baaea1d434071ca290ea3e96b11e5ed5f
HAASINI72 <haasi.email.123@gmail.com>
2025-07-19 23:14:10 +0530

    add

--- CV10_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/4_haasini.py
+++ CV10_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/4_haasini.py
@@ -1,6 +1,4 @@
 import pywhatkit
 
-pywhatkit.image_to_ascii_art(
-    r'C:\Users\jeeva\OneDrive\Documents\Haasi\CODEVERSE\FLOWER.jpg',
-    r'C:\Users\jeeva\OneDrive\Documents\Haasi\CODEVERSE\ascii_output'
-)
+# Convert an image to ASCII art
+pywhatkit.image_to_ascii_art('C:/CodeVerse/Hands on python/2_1_2_pywhatkit_Automation/FLOWER.jpg', 'C:/CodeVerse/Hands on python/2_1_2_pywhatkit_Automation/ascii_output')
