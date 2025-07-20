d09cb9a12ad352a7bbbb82bed665caac8ef9e5c3
tanu2908 <tanushreegowda8433@gmail.com>
2025-07-20 12:13:46 +0530

    Create 2_tanushree.py

---
+++ CV25_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/Pywhatkit_Whatsapp_message_generator/2_tanushree.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+# Same as above but Closes the Tab in 60 Seconds after Sending the Message
+pywhatkit.sendwhatmsg("+919972170225", "Hi", 9, 22
+                      , 15, True, 15)
+#
