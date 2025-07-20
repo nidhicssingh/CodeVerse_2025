7ee7a51409d424d5c5f850f6afe127d793c0755a
gousiya05 <gousiya1405@gmail.com>
2025-07-20 09:24:50 +0530

    Add files via upload

---
+++ CV27_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/1_whatsapp_msg.py
@@ -0,0 +1,6 @@
+import pywhatkit
+ 
+# Same as above but Closes the Tab in 60 Seconds after Sending the Message
+pywhatkit.sendwhatmsg("+919483858750", "Hi, this side gousiya.", 9, 26
+                      , 15, True, 15)
+#
