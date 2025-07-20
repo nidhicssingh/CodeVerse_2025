0bceadbaca373fee1d0ef79aaea781a019bf0352
Chinmayi-gururaj-36 <chinmayi.gc13@gmail.com>
2025-07-20 16:03:31 +0530

    Add files via upload

---
+++ CV29_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/3_Chinmayi4.py.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+# Convert an image to ASCII art
+pywhatkit.image_to_ascii_art('"C:\Users\chinm\Downloads\pywhatkit_Automation-20250720T084149Z-1-001\pywhatkit_Automation\FLOWER.jpg"', 'E:/CodeVerse/Hands on python/2_1_2_pywhatkit_Automation/ascii_output')
