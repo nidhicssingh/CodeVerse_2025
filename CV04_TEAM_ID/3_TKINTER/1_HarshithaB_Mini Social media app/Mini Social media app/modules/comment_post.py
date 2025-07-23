36cd7879da30880b053a858a229a71ad719164cb
Harshitha-software <harshithashilpabhaskar@gmail.com>
2025-07-23 20:11:00 +0530

    Add files via upload

---
+++ CV04_TEAM_ID/3_TKINTER/1_HarshithaB_Mini Social media app/Mini Social media app/modules/comment_post.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import simpledialog
+
+def comment_window(root, post):
+    comment = simpledialog.askstring("Comment", "Write your comment:")
+    if comment:
+        post["comments"].append({"user": "Harshitha", "text": comment})
