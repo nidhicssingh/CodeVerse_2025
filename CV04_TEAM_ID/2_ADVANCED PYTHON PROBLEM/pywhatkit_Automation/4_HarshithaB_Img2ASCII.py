8cc813dbd69b1b705bc8cf61164e05147b6f3127
Harshitha-software <harshithashilpabhaskar@gmail.com>
2025-07-23 17:15:28 +0530

    Create 4_HarshithaB_Img2ASCII.py

---
+++ CV04_TEAM_ID/2_ADVANCED PYTHON PROBLEM/pywhatkit_Automation/4_HarshithaB_Img2ASCII.py
@@ -0,0 +1,6 @@
+from pywhatkit import image_to_ascii_art
+
+image_to_ascii_art(
+    r'C:\Users\harshitha\Documents\Pywhatit\Pywhatit_Automation\Flower.jpg',
+    r'C:\Users\harshitha\Documents\Pywhatit\Pywhatit_Automation\ascii_output'
+)
