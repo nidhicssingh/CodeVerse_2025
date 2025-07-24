bf473723874310beef6dda0dee45961a4932818e
shreyaashekhar05 <shreyaashekhar05@gmail.com>
2025-07-24 14:17:34 +0530

    Add files via upload

---
+++ CV30_TEAM_ID/5_TURTE_PYTHON/import turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("pink")
+
+
+t=turtle.Turtle()
+t.color("red","light green")
+t.pensize(3)
+
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+
+turtle.done()
+
