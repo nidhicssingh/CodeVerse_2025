2ed7574756ab5b206e9bee1ff3ab3a9ef9fb7866
Zaina282 <zainamanal1190@gmail.com>
2025-07-25 00:23:46 +0530

    Add files via upload

---
+++ CV23_TEAM_ID/6_DJANGO/zaina portfolio/Portfolio/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from Nidhi import views
+
+urlpatterns = [
+     path("", views.index, name='base'),
+     path('about',views.about, name='about'),
+     path('services',views.services, name='services'),
+     path('contact',views.contact, name='contact'),
+     path('family',views.family, name='family'),
+     path('skills',views.skills, name='skills'),
+     path('education',views.education, name='education'),
+     path('blogs',views.blogs, name='blogs'),
+     path('projects',views.projects, name='projects'),
+     path('certification', views.certification, name='certification'),
+     path('featured', views.featured, name='featured'),
+]
