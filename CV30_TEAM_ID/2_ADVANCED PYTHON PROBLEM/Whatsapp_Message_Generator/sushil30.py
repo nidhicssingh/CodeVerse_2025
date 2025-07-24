504462c2864a7cbebb0e926d34a5c69469cfab80
Sushil S Mudenur <sushilsmudenur2006@gmail.com>
2025-07-24 18:05:23 +0530

    Add files via upload

---
+++ CV30_TEAM_ID/2_ADVANCED PYTHON PROBLEM/Whatsapp_Message_Generator/sushil30.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+# Same as above but Closes the Tab in 60 Seconds after Sending the Message
+pywhatkit.sendwhatmsg("+916361158067", "Hi", 9, 22
+                      , 15, True, 15)
+#
