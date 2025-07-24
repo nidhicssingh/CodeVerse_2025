fdced657f74d084ac6bcfd79fcac585f7ee0f8ea
Sushil S Mudenur <sushilsmudenur2006@gmail.com>
2025-07-24 15:55:06 +0530

    Add files via upload

---
+++ CV30_TEAM_ID/5_TURTE_PYTHON/sushil22.py
@@ -0,0 +1,15 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("pink")
+
+
+t=turtle.Turtle()
+t.color("red","light green")
+t.pensize(3)
+
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+
+turtle.done()
