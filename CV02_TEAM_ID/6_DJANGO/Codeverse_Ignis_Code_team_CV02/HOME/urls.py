45376fff0de319bb495bc83a1b5d85d453a42b36
A Jeffrey D'souza <jeffreydsouza5002@gmail.com>
2025-07-25 22:12:55 +0530

    Add files via upload

---
+++ CV02_TEAM_ID/6_DJANGO/Codeverse_Ignis_Code_team_CV02/HOME/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('featured/', views.featured, name='featured'),
+    # other paths...
+]
+urlpatterns = [
+     path("", views.home, name='home'),
+     path('about',views.about, name='about'),
+     path('services',views.services, name='services'),
+     path('contact',views.contact, name='contact'),
+     path('certification',views.certification, name='certification'),
+     path('skills',views.skills, name='skills'),
+     path('education',views.education, name='education'),
+     path('blogs',views.blogs, name='blogs'),
+     path('projects',views.projects, name='projects'),
+     
+     
+     
+     ]
