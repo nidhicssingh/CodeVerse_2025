bc04d4f896eefe43f0c0d2d39268d1af518ef1ee
Sushma895 <sushmabhattarai2006@gmail.com>
2025-07-25 23:27:53 +0530

    Add files via upload

---
+++ CV15_TEAM_ID/3_TKINTER/Python quiz_Poornima/subjects.py
@@ -0,0 +1,76 @@
+# subjects.py
+
+Math_quiz = {
+    "quiz_data": [
+        {
+            "question": "What is 5 + 7?",
+            "options": ["10", "11", "12", "13"],
+            "answer": "12"
+        },
+        {
+            "question": "What is the square root of 81?",
+            "options": ["9", "8", "7", "6"],
+            "answer": "9"
+        }
+    ]
+}
+
+Data_structure_quiz = {
+    "quiz_data": [
+        {
+            "question": "Which data structure uses LIFO?",
+            "options": ["Queue", "Stack", "Array", "Tree"],
+            "answer": "Stack"
+        },
+        {
+            "question": "Which is not a linear data structure?",
+            "options": ["Array", "Linked List", "Tree", "Queue"],
+            "answer": "Tree"
+        }
+    ]
+}
+
+Python_quiz = {
+    "quiz_data": [
+        {
+            "question": "What does 'len()' do in Python?",
+            "options": ["Returns size", "Sorts data", "Adds numbers", "None"],
+            "answer": "Returns size"
+        },
+        {
+            "question": "Which keyword defines a function?",
+            "options": ["def", "func", "define", "lambda"],
+            "answer": "def"
+        }
+    ]
+}
+
+GK_quiz = {
+    "quiz_data": [
+        {
+            "question": "Who is the Prime Minister of India (2024)?",
+            "options": ["Narendra Modi", "Rahul Gandhi", "Amit Shah", "Arvind Kejriwal"],
+            "answer": "Narendra Modi"
+        },
+        {
+            "question": "Which is the capital of Australia?",
+            "options": ["Sydney", "Melbourne", "Canberra", "Perth"],
+            "answer": "Canberra"
+        }
+    ]
+}
+
+CN_quiz = {
+    "quiz_data": [
+        {
+            "question": "What does IP stand for?",
+            "options": ["Internet Protocol", "Internal Protocol", "Input Process", "Interconnect Program"],
+            "answer": "Internet Protocol"
+        },
+        {
+            "question": "Which device connects two networks?",
+            "options": ["Switch", "Hub", "Router", "Repeater"],
+            "answer": "Router"
+        }
+    ]
+}
